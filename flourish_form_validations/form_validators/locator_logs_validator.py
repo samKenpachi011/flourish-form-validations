637254bee5cdb03dcb13f8704c8e7302002bd7c0
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2020-11-26 16:11:52 +0200

    Validate locator log entry

---
+++ flourish_form_validations/form_validators/locator_logs_validator.py
@@ -0,0 +1,13 @@
+from edc_form_validators import FormValidator
+
+
+class LocatorLogEntryFormValidator(FormValidator):
+
+    def clean(self):
+        super().clean()
+
+        self.required_if(
+            'not_found',
+            field='log_status',
+            field_required='comment',
+            required_msg='Please provde a short brief/reason')
