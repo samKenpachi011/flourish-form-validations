48fdfdd81dd188d46055bb702540a473797aacc5
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2021-02-01 15:22:48 +0200

    Validate clinical measurements form

--- flourish_form_validations/form_validators/in_person_contact_attempt_form_validator.py
+++ flourish_form_validations/form_validators/in_person_contact_attempt_form_validator.py
@@ -1,6 +1,3 @@
-from django.core.exceptions import ValidationError
-
-from edc_constants.constants import OTHER
 from edc_form_validators import FormValidator
 
 
@@ -16,21 +13,3 @@ class InPersonContactAttemptFormValidator(FormValidator):
 
         self.validate_other_specify(
             field='contact_person_unsuc')
-
-        contact_location = self.cleaned_data.get('contact_location')
-        successful_location = self.cleaned_data.get('successful_location')
-
-        phy_addr_unsuc = self.cleaned_data.get('phy_addr_unsuc')
-        workplace_unsuc = self.cleaned_data.get('workplace_unsuc')
-        contact_person_unsuc = self.cleaned_data.get('contact_person_unsuc')
-
-        if contact_location != successful_location:
-
-            required_fields = [phy_addr_unsuc, workplace_unsuc, contact_person_unsuc]
-
-            for field in required_fields:
-                if field == '':
-                    message = {'phy_addr_unsuc':
-                               'This field is required'}
-                    self._errors.update(message)
-                    raise ValidationError(message)
