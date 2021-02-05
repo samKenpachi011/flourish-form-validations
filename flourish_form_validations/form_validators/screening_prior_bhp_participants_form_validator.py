88f8786d431313d095dd0610b4906ac24af2e5c6
ame diphoko <diphokoame@gmail.com>
2021-02-05 16:58:52 +0200

    update screening prior validations

--- flourish_form_validations/form_validators/screening_prior_bhp_participants_form_validator.py
+++ flourish_form_validations/form_validators/screening_prior_bhp_participants_form_validator.py
@@ -1,56 +1,34 @@
 from django.core.exceptions import ValidationError
 from edc_form_validators import FormValidator
-from edc_constants.constants import NO, YES, UNKNOWN
+from edc_constants.constants import NO, UNKNOWN, NOT_APPLICABLE
 
 
 class ScreeningPriorBhpParticipantsFormValidator(FormValidator):
 
     def clean(self):
-
         self.validate_mother_alive()
-        self.validate_age_assurance()
+        self.validate_participation()
 
     def validate_mother_alive(self):
-        mother_alive_options = [NO, UNKNOWN]
-        mother_alive = self.cleaned_data.get('mother_alive')
-
-        if mother_alive in mother_alive_options:
-
-            self.required_if(
-                NO,
-                field='mother_alive',
-                field_required='flourish_interest',
-                inverse=False)
-
-            self.required_if(
-                UNKNOWN,
-                field='mother_alive',
-                field_required='flourish_interest',
-                inverse=False)
-        else:
-            self.not_required_if(
-                YES,
-                field='mother_alive',
-                field_required='flourish_interest',
-                inverse=False)
-            self.not_required_if(
-                YES,
-                field='mother_alive',
-                field_required='age_assurance',
-                inverse=False)
-
-    def validate_age_assurance(self):
-
-        age_assurance = self.cleaned_data.get('age_assurance')
-
-        if age_assurance == NO:
-            message = {'flourish_interest':
-                       'Interested Caregiver Is Under-Age'}
-            self._errors.update(message)
-            raise ValidationError(message)
 
         self.required_if(
-            YES,
-            field='flourish_interest',
-            field_required='age_assurance',
-            inverse=False)
+            *[NO, UNKNOWN],
+            field='mother_alive',
+            field_required='flourish_interest')
+
+    def validate_participation(self):
+        cleaned_data = self.cleaned_data
+        mother_alive = cleaned_data.get('mother_alive')
+        flourish_interest = cleaned_data.get('flourish_interest')
+        if mother_alive == NO and flourish_interest == NO:
+            if cleaned_data.get('flourish_participation') != NOT_APPLICABLE:
+                message = {'flourish_participation':
+                           'This field is not applicable'}
+                self._errors.update(message)
+                raise ValidationError(message)
+        else:
+            if cleaned_data.get('flourish_participation') == NOT_APPLICABLE:
+                message = {'flourish_participation':
+                           'This field is applicable'}
+                self._errors.update(message)
+                raise ValidationError(message)
