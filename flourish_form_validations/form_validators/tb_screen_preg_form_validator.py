714c36d40abe80cb46c9d4e51956f976888c0a72
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2021-02-09 07:30:46 +0200

    Update tb screen for pregnant women validator

--- flourish_form_validations/form_validators/tb_screen_preg_form_validator.py
+++ flourish_form_validations/form_validators/tb_screen_preg_form_validator.py
@@ -7,15 +7,7 @@ class TbScreenPregFormValidator(FormValidator):
     def clean(self):
         self.required_if(
             YES,
-            field='have_cough',
-            field_required='cough_lasted_2wks')
+            field='tb_screened',
+            field_required='where_screened')
 
-        self.required_if(
-            YES,
-            field='have_fever',
-            field_required='fever_lasted_2wks')
-
-        self.required_if(
-            YES,
-            field='have_night_sweats',
-            field_required='sweats_lasted_2wks')
+        self.validate_other_specify(field='where_screened')
