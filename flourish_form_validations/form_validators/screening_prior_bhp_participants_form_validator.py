8ce97ff825e3955b49dc0cb96c3c11479a418b60
ame diphoko <diphokoame@gmail.com>
2021-02-16 09:40:12 +0200

    update screening validations

--- flourish_form_validations/form_validators/screening_prior_bhp_participants_form_validator.py
+++ flourish_form_validations/form_validators/screening_prior_bhp_participants_form_validator.py
@@ -6,6 +6,7 @@ from edc_constants.constants import NO, UNKNOWN, NOT_APPLICABLE
 class ScreeningPriorBhpParticipantsFormValidator(FormValidator):
 
     def clean(self):
+        self.validate_child_alive()
         self.validate_mother_alive()
         self.validate_participation()
 
@@ -20,15 +21,20 @@ class ScreeningPriorBhpParticipantsFormValidator(FormValidator):
         cleaned_data = self.cleaned_data
         mother_alive = cleaned_data.get('mother_alive')
         flourish_interest = cleaned_data.get('flourish_interest')
-        if mother_alive == NO and flourish_interest == NO:
+        if mother_alive in [NO, UNKNOWN] and flourish_interest == NO:
             if cleaned_data.get('flourish_participation') != NOT_APPLICABLE:
                 message = {'flourish_participation':
                            'This field is not applicable'}
                 self._errors.update(message)
                 raise ValidationError(message)
-        else:
-            if cleaned_data.get('flourish_participation') == NOT_APPLICABLE:
-                message = {'flourish_participation':
-                           'This field is applicable'}
-                self._errors.update(message)
-                raise ValidationError(message)
+
+    def validate_child_alive(self):
+        self.not_applicable_if(
+            NO,
+            field='child_alive',
+            field_applicable='mother_alive')
+
+        self.not_required_if(
+            NO,
+            field='child_alive',
+            field_required='flourish_interest')
