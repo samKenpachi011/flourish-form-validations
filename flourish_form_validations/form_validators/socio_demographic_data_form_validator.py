c21ca870c47f47f1d2bd51bc70cedf3b57d1ead2
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2021-02-23 10:24:10 +0200

    Refine socio demographic data form validator

--- flourish_form_validations/form_validators/socio_demographic_data_form_validator.py
+++ flourish_form_validations/form_validators/socio_demographic_data_form_validator.py
@@ -9,29 +9,12 @@ class SocioDemographicDataFormValidator(CRFFormValidator, FormValidator):
     def clean(self):
         super().clean()
 
-        self.validate_other_specify(
-            field='marital_status',
-        )
+        other_specify_fields = ['marital_status', 'ethnicity',
+                                'current_occupation', 'provides_money',
+                                'money_earned', 'toilet_facility']
 
-        self.validate_other_specify(
-            field='ethnicity',
-        )
-
-        self.validate_other_specify(
-            field='current_occupation',
-        )
-
-        self.validate_other_specify(
-            field='provides_money',
-        )
-
-        self.validate_other_specify(
-            field='money_earned',
-        )
-
-        self.validate_other_specify(
-            field='toilet_facility',
-        )
+        for field in other_specify_fields:
+            self.validate_other_specify(field=field)
 
         required_fields = ['own_phone', 'water_source', 'house_electrified',
                            'house_fridge', 'cooking_method', 'toilet_facility', ]
