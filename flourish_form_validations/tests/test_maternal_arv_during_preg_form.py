7b47be3c8b7d53aaf7a71e2563347c49703d44d9
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2021-02-25 10:26:49 +0200

    Test maternal arv during preg form

---
+++ flourish_form_validations/tests/test_maternal_arv_during_preg_form.py
@@ -0,0 +1,105 @@
+from dateutil.relativedelta import relativedelta
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+from edc_base.utils import get_utcnow
+from edc_constants.constants import YES, NO, NOT_APPLICABLE
+from ..form_validators import MaternalArvDuringPregFormValidator
+from .models import MaternalVisit, Appointment, MaternalArvDuringPreg
+from .models import MaternalArvPrePreg, SubjectScreening, SubjectConsent
+
+
+class TestMaternalArvDuringPregForm(TestCase):
+
+    def setUp(self):
+        MaternalArvDuringPregFormValidator.maternal_consent_model = \
+            'flourish_form_validations.subjectconsent'
+        MaternalArvDuringPregFormValidator.subject_screening_model = \
+            'flourish_form_validations.subjectscreening'
+
+        self.subject_identifier = '11111111'
+
+        self.subject_screening = SubjectScreening.objects.create(
+            subject_identifier='11111111',
+            screening_identifier='ABC12345',
+            age_in_years=22)
+
+        self.subject_consent = SubjectConsent.objects.create(
+            subject_identifier='11111111', screening_identifier='ABC12345',
+            gender='M', dob=(get_utcnow() - relativedelta(years=25)).date(),
+            consent_datetime=get_utcnow(),)
+
+        self.appointment = Appointment.objects.create(
+            subject_identifier=self.subject_consent.subject_identifier,
+            appt_datetime=get_utcnow(),
+            visit_code='1000M')
+
+        self.maternal_visit = MaternalVisit.objects.create(
+            appointment=self.appointment,
+            subject_identifier=self.subject_consent.subject_identifier)
+
+        self.maternal_arv_during_preg = MaternalArvDuringPreg.objects.create(
+            maternal_visit=self.maternal_visit,)
+
+        # self.maternal_arv_pre_preg = MaternalArvPrePreg.objects.create(
+        #     maternal_visit=self.maternal_visit)
+
+    def test_medication_interrupted_invalid(self):
+        '''Assert raises if arvs was interrupted but
+        no reasons were given.
+        '''
+        cleaned_data = {
+            'maternal_visit': self.maternal_visit,
+            'is_interrupt': YES,
+            'interrupt': 'reason',
+        }
+        form_validator = MaternalArvDuringPregFormValidator(
+            cleaned_data=cleaned_data)
+        try:
+            form_validator.validate()
+        except ValidationError as e:
+            self.fail(f'ValidationError unexpectedly raised. Got{e}')
+
+    def test_medication_interrupted_invalid_yes(self):
+        '''Assert raises if arvs was interrupted but
+        no reason was given.
+        '''
+        cleaned_data = {
+            'maternal_visit': self.maternal_visit,
+            'is_interrupt': YES,
+            'interrupt': NOT_APPLICABLE,
+        }
+        form_validator = MaternalArvDuringPregFormValidator(
+            cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('interrupt', form_validator._errors)
+
+    def test_medication_interrupted_valid(self):
+        '''True if arvs was not interrupted and no
+        interrupt reason was provided.
+        '''
+        cleaned_data = {
+            'maternal_visit': self.maternal_visit,
+            'is_interrupt': NO,
+            'interrupt': NOT_APPLICABLE,
+            'report_datetime': get_utcnow()
+        }
+        form_validator = MaternalArvDuringPregFormValidator(
+            cleaned_data=cleaned_data)
+        try:
+            form_validator.validate()
+        except ValidationError as e:
+            self.fail(f'ValidationError raised. Got {e}')
+
+    def test_medication_interrupted_invalid_none(self):
+        '''Assert raises if no interruptions but
+        reason is given.
+        '''
+        cleaned_data = {
+            'maternal_visit': self.maternal_visit,
+            'is_interrupt': NO,
+            'interrupt': 'reason',
+        }
+        form_validator = MaternalArvDuringPregFormValidator(
+            cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('interrupt', form_validator._errors)
