30664aeba352d1d219feabd19f721701a692b8dc
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2021-03-11 12:21:25 +0200

    Update hiv viral load cd4 count validations

--- flourish_form_validations/form_validators/hiv_viralload_cd4_form_validator.py
+++ flourish_form_validations/form_validators/hiv_viralload_cd4_form_validator.py
@@ -6,10 +6,12 @@ class HivViralLoadCd4FormValidator(FormValidator):
 
     def clean(self):
 
-        self.required_if(
-            YES,
-            field='last_cd4_count_known',
-            field_required='cd4_count')
+        required_fields = ['cd4_count', 'cd4_count_date']
+        for field in required_fields:
+            self.required_if(
+                YES,
+                field='last_cd4_count_known',
+                field_required=field)
 
         self.required_if(
             YES,
