2ab814cdfc9f539121dc43e071f7e329aae05b33
tumiemosweu <imosweuk@gmail.com>
2021-03-22 19:52:37 +0200

    update consent validations

--- flourish_form_validations/form_validators/subject_consent_form_validation.py
+++ flourish_form_validations/form_validators/subject_consent_form_validation.py
@@ -78,29 +78,26 @@ class SubjectConsentFormValidator(FormValidator):
         first_name = cleaned_data.get("first_name")
         last_name = cleaned_data.get("last_name")
 
-        if first_name:
-            if not re.match(r'^[A-Z]+$|^([A-Z]+[ ][A-Z]+)$', first_name):
-                message = {'first_name': 'Ensure first name is letters (A-Z) in '
-                           'upper case, no special characters, except spaces.'}
-                self._errors.update(message)
-                raise ValidationError(message)
+        if first_name and not re.match(r'^[A-Z]+$|^([A-Z]+[ ][A-Z]+)$', first_name):
+            message = {'first_name': 'Ensure first name is letters (A-Z) in '
+                       'upper case, no special characters, except spaces.'}
+            self._errors.update(message)
+            raise ValidationError(message)
 
-        if last_name:
-            if not re.match(r'^[A-Z-]+$', last_name):
-                message = {'last_name': 'Ensure last name is letters (A-Z) in '
-                           'upper case, no special characters, except hyphens.'}
-                self._errors.update(message)
-                raise ValidationError(message)
+        if last_name and not re.match(r'^[A-Z-]+$', last_name):
+            message = {'last_name': 'Ensure last name is letters (A-Z) in '
+                       'upper case, no special characters, except hyphens.'}
+            self._errors.update(message)
+            raise ValidationError(message)
 
-        if first_name and last_name:
-            if first_name != first_name.upper():
-                message = {'first_name': 'First name must be in CAPS.'}
-                self._errors.update(message)
-                raise ValidationError(message)
-            elif last_name != last_name.upper():
-                message = {'last_name': 'Last name must be in CAPS.'}
-                self._errors.update(message)
-                raise ValidationError(message)
+        if first_name and first_name != first_name.upper():
+            message = {'first_name': 'First name must be in CAPS.'}
+            self._errors.update(message)
+            raise ValidationError(message)
+        if last_name and last_name != last_name.upper():
+            message = {'last_name': 'Last name must be in CAPS.'}
+            self._errors.update(message)
+            raise ValidationError(message)
 
     def clean_initials_with_full_name(self):
         cleaned_data = self.cleaned_data
@@ -111,13 +108,13 @@ class SubjectConsentFormValidator(FormValidator):
             middle_name = None
             is_first_name = False
             new_first_name = None
-            if len(first_name.split(' ')) > 1:
+            if first_name and len(first_name.split(' ')) > 1:
                 new_first_name = first_name.split(' ')[0]
                 middle_name = first_name.split(' ')[1]
 
-            if (middle_name and
-                (initials[:1] != new_first_name[:1] or
-                 initials[1:2] != middle_name[:1])):
+            if middle_name and (middle_name and
+                                (initials[:1] != new_first_name[:1] or
+                                 initials[1:2] != middle_name[:1])):
                 is_first_name = True
 
             elif not middle_name and initials[:1] != first_name[:1]:
