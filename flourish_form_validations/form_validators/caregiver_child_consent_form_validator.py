3552ebbc666226ed8fbf27f4996502dc7351f91e
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2021-03-24 17:53:23 +0200

    Update caregiver child consent form validator

--- flourish_form_validations/form_validators/caregiver_child_consent_form_validator.py
+++ flourish_form_validations/form_validators/caregiver_child_consent_form_validator.py
@@ -1,7 +1,8 @@
 import re
+import datetime
 from django.core.exceptions import ValidationError
 
-from edc_constants.choices import FEMALE, MALE, NO
+from edc_constants.choices import FEMALE, MALE, YES, NO, NOT_APPLICABLE
 from edc_form_validators import FormValidator
 
 
@@ -12,13 +13,10 @@ class CaregiverChildConsentFormValidator(FormValidator):
         self.subject_identifier = self.cleaned_data.get('subject_identifier')
         super().clean()
 
-        self.required_if(
-            NO,
-            field='is_literate',
-            field_required='witness_name')
-
         self.clean_full_name_syntax()
         self.validate_identity_number(cleaned_data=self.cleaned_data)
+        self.validate_child_knows_status(cleaned_data=self.cleaned_data)
+        self.validate_child_preg_test(cleaned_data=self.cleaned_data)
 
     def clean_full_name_syntax(self):
         cleaned_data = self.cleaned_data
@@ -63,11 +61,44 @@ class CaregiverChildConsentFormValidator(FormValidator):
             gender = cleaned_data.get('gender')
             if gender == FEMALE and cleaned_data.get('identity')[4] != '2':
                 msg = {'identity':
-                       'Participant gender is Female. Please correct identity number.'}
+                       'Child gender is Female. Please correct identity number.'}
                 self._errors.update(msg)
                 raise ValidationError(msg)
             elif gender == MALE and cleaned_data.get('identity')[4] != '1':
                 msg = {'identity':
-                       'Participant is Male. Please correct identity number.'}
+                       'Child is Male. Please correct identity number.'}
                 self._errors.update(msg)
                 raise ValidationError(msg)
+
+    def validate_child_preg_test(self, cleaned_data=None):
+        if cleaned_data.get('gender') == 'M' and cleaned_data.get(
+                'child_preg_test') in [YES, NO]:
+            msg = {'child_preg_test':
+                   'Can only be answered as Not applicable since child is Male'}
+            self._errors.update(msg)
+            raise ValidationError(msg)
+        elif cleaned_data.get('gender') == 'F' and cleaned_data.get(
+                'child_preg_test') == NOT_APPLICABLE:
+            msg = {'child_preg_test':
+                   'Child is Female. This field is applicable'}
+            self._errors.update(msg)
+            raise ValidationError(msg)
+
+    def validate_child_knows_status(self, cleaned_data):
+        today = datetime.date.today()
+        child_dob = cleaned_data.get('child_dob')
+        child_age = (today.year - child_dob.year -
+                     ((today.month, today.day) <
+                      (child_dob.month, child_dob.day)))
+        if child_age < 16 and cleaned_data.get(
+                'child_knows_status') in [YES, NO]:
+            msg = {'child_knows_status':
+                   'Child is less than 16 years'}
+            self._errors.update(msg)
+            raise ValidationError(msg)
+        elif child_age >= 16 and cleaned_data.get(
+                'child_knows_status') == NOT_APPLICABLE:
+            msg = {'child_knows_status':
+                   'This field is applicable'}
+            self._errors.update(msg)
+            raise ValidationError(msg)
