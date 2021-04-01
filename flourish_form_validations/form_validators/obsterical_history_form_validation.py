5b0f860053290801735282368bd33275d3e0af2c
tumiemosweu <imosweuk@gmail.com>
2021-04-01 10:50:46 +0200

    remove ultrasound validation on obstetrical history

--- flourish_form_validations/form_validators/obsterical_history_form_validation.py
+++ flourish_form_validations/form_validators/obsterical_history_form_validation.py
@@ -17,7 +17,7 @@ class ObstericalHistoryFormValidator(CRFFormValidator, FormValidator):
             'maternal_visit').subject_identifier
         super().clean()
 
-        self.validate_ultrasound(cleaned_data=self.cleaned_data)
+        # self.validate_ultrasound(cleaned_data=self.cleaned_data)
         self.validate_prev_pregnancies(cleaned_data=self.cleaned_data)
         self.validate_children_deliv(cleaned_data=self.cleaned_data)
 
@@ -34,21 +34,21 @@ class ObstericalHistoryFormValidator(CRFFormValidator, FormValidator):
                                cleaned_data.get('lost_after_24wks'))
             if (cleaned_data.get('prev_pregnancies') and
                 sum_deliv_37_wks != ((cleaned_data.get('prev_pregnancies') - 1)
-                                     - sum_lost_24_wks)):
+                                     -sum_lost_24_wks)):
                 raise ValidationError('The sum of Q9 and Q10 must be equal to '
                                       '(Q3 - 1) - (Q5 + Q6). Please correct.')
 
     def validate_prev_pregnancies(self, cleaned_data=None):
-        ultrasound = self.ultrasound_model_cls.objects.filter(
-            maternal_visit=cleaned_data.get('maternal_visit'))
-        if not ultrasound:
-            message = {'__all__': 'Please complete the ultrasound form first'}
-            self._errors.update(message)
-            raise ValidationError(message)
+        # ultrasound = self.ultrasound_model_cls.objects.filter(
+            # maternal_visit=cleaned_data.get('maternal_visit'))
+        # if not ultrasound:
+            # message = {'__all__': 'Please complete the ultrasound form first'}
+            # self._errors.update(message)
+            # raise ValidationError(message)
 
         if (('prev_pregnancies' in cleaned_data and
-                cleaned_data.get('prev_pregnancies') > 1)
-                or ultrasound[0].ga_confirmed >= 24):
+                cleaned_data.get('prev_pregnancies') > 1)):
+                # or ultrasound[0].ga_confirmed >= 24):
             if (cleaned_data.get('pregs_24wks_or_more') and
                     cleaned_data.get('lost_before_24wks')):
                 sum_pregs = (cleaned_data.get('pregs_24wks_or_more') +
@@ -69,24 +69,24 @@ class ObstericalHistoryFormValidator(CRFFormValidator, FormValidator):
                     self._errors.update(message)
                     raise ValidationError(message)
 
-    def validate_ultrasound(self, cleaned_data=None):
-        ultrasound = self.ultrasound_model_cls.objects.filter(
-            maternal_visit=cleaned_data.get('maternal_visit'))
-        if not ultrasound:
-            message = {'__all__': 'Please complete the ultrasound form first'}
-            self._errors.update(message)
-            raise ValidationError(message)
-
-        if (cleaned_data.get('prev_pregnancies') == 1 and
-                ultrasound[0].ga_confirmed < 24):
-            fields = ['pregs_24wks_or_more',
-                      'lost_before_24wks', 'lost_after_24wks']
-            for field in fields:
-                if (field in cleaned_data and
-                        cleaned_data.get(field) != 0):
-                    message = {field: 'You indicated previous pregnancies were'
-                                      ' %s, %s should be zero' %
-                               (cleaned_data.get('prev_pregnancies'),
-                                field)}
-                    self._errors.update(message)
-                    raise ValidationError(message)
+    # def validate_ultrasound(self, cleaned_data=None):
+        # ultrasound = self.ultrasound_model_cls.objects.filter(
+            # maternal_visit=cleaned_data.get('maternal_visit'))
+        # if not ultrasound:
+            # message = {'__all__': 'Please complete the ultrasound form first'}
+            # self._errors.update(message)
+            # raise ValidationError(message)
+            #
+        # if (cleaned_data.get('prev_pregnancies') == 1 and
+                # ultrasound[0].ga_confirmed < 24):
+            # fields = ['pregs_24wks_or_more',
+                      # 'lost_before_24wks', 'lost_after_24wks']
+            # for field in fields:
+                # if (field in cleaned_data and
+                        # cleaned_data.get(field) != 0):
+                    # message = {field: 'You indicated previous pregnancies were'
+                                      # ' %s, %s should be zero' %
+                               # (cleaned_data.get('prev_pregnancies'),
+                                # field)}
+                    # self._errors.update(message)
+                    # raise ValidationError(message)
