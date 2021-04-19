3afc0975fbc58cf026cfb6213a8938c3dfd7b290
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2021-04-19 09:56:12 +0200

    Update HIV Disclosure Form validator

--- flourish_form_validations/form_validators/hiv_disclosure_status_form_validator.py
+++ flourish_form_validations/form_validators/hiv_disclosure_status_form_validator.py
@@ -1,3 +1,4 @@
+from edc_constants.choices import YES
 from edc_form_validators import FormValidator
 
 
@@ -5,5 +6,12 @@ class HIVDisclosureStatusFormValidator(FormValidator):
 
     def clean(self):
         super().clean()
+
+        not_required_fields = ['plan_to_disclose', 'reason_not_disclosed',
+                               'reason_not_disclosed_other']
+        for field in not_required_fields:
+            self.not_required_if(YES,
+                                 field='disclosed_status',
+                                 field_required=field)
 
         self.validate_other_specify(field='reason_not_disclosed')
