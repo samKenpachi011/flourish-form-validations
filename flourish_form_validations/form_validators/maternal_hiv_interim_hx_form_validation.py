057445f93a2e5f5f8957680677aaac1a1593590e
Moses Chawawa <moseschawawa@gmail.com>
2021-08-13 11:25:05 +0200

    Added a null check for vl_result ig

--- flourish_form_validations/form_validators/maternal_hiv_interim_hx_form_validation.py
+++ flourish_form_validations/form_validators/maternal_hiv_interim_hx_form_validation.py
@@ -47,7 +47,16 @@ class MaternalHivInterimHxFormValidator(CRFFormValidator,
         """
         # Get data fro the form and convert to on integer
         vl_detectable = self.cleaned_data.get('vl_detectable')
-        vl_result = int(self.cleaned_data.get('vl_result'))
+        vl_result = self.cleaned_data.get('vl_result')
+
+        # Check if the vl_result is null
+        if vl_detectable == YES and vl_result is None:
+            raise ValidationError({'vl_result', 'Cannot be empty'})
+        if vl_detectable == NO and vl_result is None:
+            raise ValidationError({'vl_result', 'Cannot be empty'})
+
+        # if not null, then convert vl_resut to int through reassignment
+        vl_result = int(vl_result)
 
         # This is the original required condition, Superposed for readability
         if vl_detectable == YES and not (vl_result > 400):
