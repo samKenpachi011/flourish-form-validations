a81a97c15357001d0174cbd6487ef17defe92b14
Moses Chawawa <moseschawawa@gmail.com>
2021-09-20 17:55:49 +0200

    REMOVED: Some of the validations

--- flourish_form_validations/form_validators/antenatal_enrollment_form_validation.py
+++ flourish_form_validations/form_validators/antenatal_enrollment_form_validation.py
@@ -35,23 +35,23 @@ class AntenatalEnrollmentFormValidator(CRFFormValidator,
             field_required='rapid_test_result'
         )
 
-        self.applicable_if(
-            POS,
-            field='week32_result',
-            field_applicable='will_get_arvs'
-        )
+        # self.applicable_if(
+        #     POS,
+        #     field='week32_result',
+        #     field_applicable='will_get_arvs'
+        # )
+        #
+        # self.required_if(
+        #     YES,
+        #     field='week32_test',
+        #     field_required='week32_result'
+        # )
 
-        self.required_if(
-            YES,
-            field='week32_test',
-            field_required='week32_result'
-        )
-
-        self.applicable_if(
-            YES,
-            field='week32_test',
-            field_applicable='evidence_32wk_hiv_status'
-        )
+        # self.applicable_if(
+        #     YES,
+        #     field='week32_test',
+        #     field_applicable='evidence_32wk_hiv_status'
+        # )
 
         self.validate_last_period_date(cleaned_data=self.cleaned_data)
 
@@ -62,7 +62,7 @@ class AntenatalEnrollmentFormValidator(CRFFormValidator,
             id=id)
 
         self.validate_current_hiv_status()
-        self.validate_week32_result()
+        # self.validate_week32_result()
 
         enrollment_helper = EnrollmentHelper(
             instance_antenatal=self.antenatal_enrollment_cls(
@@ -76,29 +76,6 @@ class AntenatalEnrollmentFormValidator(CRFFormValidator,
                 'Unable to determine maternal hiv status at enrollment.')
 
         enrollment_helper.raise_validation_error_for_rapidtest()
-
-    def validate_week32_result(self):
-        if (self.cleaned_data.get('rapid_test_done') == NO and
-                self.cleaned_data.get('current_hiv_status') != self.cleaned_data.get(
-                    'week32_result')):
-            message = {'current_hiv_status':
-                       'Current HIV status must match HIV test result. Please'
-                       ' correct.'}
-            self._errors.update(message)
-            raise ValidationError(message)
-        elif self.cleaned_data.get('rapid_test_done') == YES:
-            if (self.cleaned_data.get('rapid_test_result') !=
-                    self.cleaned_data.get('current_hiv_status')):
-                message = {'current_hiv_status':
-                           'Current  HIV status must match rapid test result.'}
-                self._errors.update(message)
-                raise ValidationError(message)
-            if (self.cleaned_data.get('rapid_test_result') !=
-                    self.cleaned_data.get('week32_result')):
-                message = {'week32_result':
-                           'HIV result must match rapid test result.'}
-                self._errors.update(message)
-                raise ValidationError(message)
 
     def validate_current_hiv_status(self):
         if (self.cleaned_data.get('week32_test') == NO and
