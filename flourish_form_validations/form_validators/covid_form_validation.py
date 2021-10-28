efb887ca8026de38096d997c7e216f397f857e22
Moses Chawawa <moseschawawa@gmail.com>
2021-10-28 11:57:07 +0200

    fields not required if not vaccinated

--- flourish_form_validations/form_validators/covid_form_validation.py
+++ flourish_form_validations/form_validators/covid_form_validation.py
@@ -46,9 +46,7 @@ class Covid9FormValidator(FormValidator):
     def _validations_if_fully_vaccinated(self):
 
         if self.cleaned_data.get('fully_vaccinated') == YES:
-
             required_fields = ['vaccination_type', 'first_dose', 'second_dose']
-
             for field in required_fields:
                 self.required_if(YES,
                                  field='fully_vaccinated',
@@ -56,6 +54,8 @@ class Covid9FormValidator(FormValidator):
 
             self.validate_other_specify(field='vaccination_type',
                                         other_specify_field='other_vaccination_type')
+        elif self.cleaned_data.get('fully_vaccinated') == NO:
+            self._validate_not_required()
 
     def _validations_if_partially_vaccinated(self):
 
@@ -68,7 +68,19 @@ class Covid9FormValidator(FormValidator):
                                  field='fully_vaccinated',
                                  field_required=field)
 
-        self.validate_other_specify(field='vaccination_type',
-                                    other_specify_field='other_vaccination_type')
+            self.validate_other_specify(field='vaccination_type',
+                                        other_specify_field='other_vaccination_type')
 
-        self.not_required_if('partially_jab', field='fully_vaccinated', field_required='second_dose')
+            self.not_required_if('partially_jab',
+                                 field='fully_vaccinated',
+                                 field_required='second_dose')
+
+        elif self.cleaned_data.get('fully_vaccinated') == NO:
+            self._validate_not_required()
+
+    def _validate_not_required(self):
+        not_required_fields = ['vaccination_type', 'other_vaccination_type', 'first_dose', 'second_dose']
+        for field in not_required_fields:
+            self.not_required_if(NO,
+                                 field='fully_vaccinated',
+                                 field_required=field)
