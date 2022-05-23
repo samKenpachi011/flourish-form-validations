eb992952b49874919a0267ecb87202f275d3784f
samKenpachi011 <samuelkabelo1@gmail.com>
2022-05-23 12:40:20 +0200

    updated clinical measurements form validator

--- flourish_form_validations/form_validators/caregiver_clinical_measurements_form_validator.py
+++ flourish_form_validations/form_validators/caregiver_clinical_measurements_form_validator.py
@@ -1,5 +1,5 @@
 from django.core.exceptions import ValidationError
-from edc_constants.constants import YES
+from edc_constants.constants import YES,NO
 from edc_form_validators import FormValidator
 
 from .crf_form_validator import FormValidatorMixin
@@ -24,6 +24,14 @@ class CaregiverClinicalMeasurementsFormValidator(FormValidatorMixin,
                        'diastolic blood pressure. Please correct.'}
                 self._errors.update(msg)
                 raise ValidationError(msg)
+            
+            
+        confirm_values = self.cleaned_data.get('confirm_values')
+        if confirm_values == NO:
+            message = {'confirm_values':
+                        'Please ensure that you agree with the given values'}
+            self._errors.update(message)
+            raise ValidationError(message)  
 
         self.required_if_true(
             (cleaned_data.get('is_preg') != YES
@@ -35,27 +43,7 @@ class CaregiverClinicalMeasurementsFormValidator(FormValidatorMixin,
                 and cleaned_data.get('maternal_visit').visit_code != '2000D'),
             field_required='hip_circ')
         
-        self.validate_systolic_bp(cleaned_data=cleaned_data)
-        self.validate_diastolic_bp(cleaned_data=cleaned_data)
-
-    def validate_systolic_bp(self,cleaned_data=None):
-        systolic_bp_upper = 130
-        systolic_bp_lower = 100
-        systolic_bp = cleaned_data.get('systolic_bp')
-
-        if systolic_bp:
-            self.required_if_true((systolic_bp < systolic_bp_lower) or (systolic_bp > systolic_bp_upper),
-                                  field_required='confirm_values')
-            
-    def validate_diastolic_bp(self,cleaned_data=None):
-        
-        diastolic_bp_upper = 80
-        diastolic_bp_lower = 60
-        diastolic_bp = cleaned_data.get('diastolic_bp')
-        
-        if diastolic_bp:
-            self.required_if_true((diastolic_bp < diastolic_bp_lower) or (diastolic_bp > diastolic_bp_upper),
-                                  field_required='confirm_values')
+  
             
         
             
