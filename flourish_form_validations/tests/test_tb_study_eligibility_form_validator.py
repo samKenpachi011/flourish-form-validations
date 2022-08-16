4067ab4638cdc57f82d546deb95969daeb28deba
McTurner45 <58169969+nmunatsi@users.noreply.github.com>
2022-08-16 13:27:02 +0200

    updating test for tb screening form and flourish consent form

--- flourish_form_validations/tests/test_tb_study_eligibility_form_validator.py
+++ flourish_form_validations/tests/test_tb_study_eligibility_form_validator.py
@@ -1,33 +1,37 @@
 from django.core.exceptions import ValidationError
+from django.core.exceptions import ValidationError
 from django.test import TestCase
-from edc_constants.constants import NO, YES, OTHER
+from edc_constants.constants import NO, OTHER, YES
 
-from ..form_validators.tb_study_eligibility_form_validator import \
-    TbStudyEligibilityFormValidator
+from ..form_validators import TbStudyEligibilityFormValidator
 
 
 class TestTbStudyEligibilityFormValidator(TestCase):
 
-
     def test_reasons_not_participating_required(self):
+        """
+        Test if  reasons for not participating are needed if the participant
+        does not want to take part in the study
+        """
         cleaned_data = {
-            'reasons_not_participating': 'YES',
-            'tb_participation': NO
+            'tb_participation': YES,
+            'reasons_not_participating': 'blah blah',
         }
 
         form_validator = TbStudyEligibilityFormValidator(
             cleaned_data=cleaned_data
         )
-
-        try:
-            form_validator.validate()
-        except ValidationError as e:
-            self.fail(f'ValidationError unexpectedly raised. Got{e}')
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('reasons_not_participating', form_validator._errors)
 
     def test_reasons_not_participating_required_other(self):
+        """
+        checks if the field for other reasons for not participating ton the study is
+        required of the participant selects their reason as `Other`
+        """
         cleaned_data = {
-            'reasons_not_participating': OTHER,
-            'tb_participation': YES,
+            'reasons_not_participating': 'Still thinking',
+            'tb_participation': NO,
             'reasons_not_participating_other': 'None'
         }
 
@@ -35,7 +39,5 @@ class TestTbStudyEligibilityFormValidator(TestCase):
             cleaned_data=cleaned_data
         )
 
-        try:
-            form_validator.validate()
-        except ValidationError as e:
-            self.fail(f'ValidationError unexpectedly raised. Got{e}')
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('reasons_not_participating_other', form_validator._errors)
