c75d72730eb09ee2a22db4fcb199dee6f7f3c52e
samKenpachi011 <samuelkabelo1@gmail.com>
2022-08-20 07:04:08 +0200

    staging

--- flourish_form_validations/form_validators/relationship_father_involvement_form_validation.py
+++ flourish_form_validations/form_validators/relationship_father_involvement_form_validation.py
@@ -1,5 +1,6 @@
+from black import re
 from edc_form_validators import FormValidator
-from edc_constants.constants import OTHER, YES, POS, NEG
+from edc_constants.constants import OTHER, YES, POS, NEG, NO
 from edc_form_validators import FormValidator
 
 from .crf_form_validator import FormValidatorMixin
@@ -7,6 +8,104 @@ from .crf_form_validator import FormValidatorMixin
 
 class RelationshipFatherInvolmentFormValidator(FormValidatorMixin,FormValidator):
     
+    
     def clean(self):
+        self.validate_why_partner_upsent_required()
+        self.validate_not_living_with_partner_required()
+        self.validate_discussion_with_partner_required()
+        self.validate_disclose_status_required()
+        self.validate_times_separated_required()
+        self.validate_separation_consideration_required()
         
-        pass
+        
+    
+        
+        
+    # On yes    
+        
+    def validate_is_partner_the_father_required(self):
+        self.required_if(YES,
+                         field='partner_present',
+                         field_required='is_partner_the_father')
+        
+    def validate_why_not_living_with_partner_required(self):
+        self.required_if(YES,
+                         field='living_with_partner',
+                         field_required='why_not_living_with_partner') 
+        
+    def validate_discussion_with_partner_required(self):
+        self.required_if(YES,
+                         field='disclosure_to_partner',
+                         field_required='discussion_with_partner')      
+        
+    def validate_separation_consideration_required(self):
+        self.required_if(YES,
+                         field='ever_separated',
+                         field_required='separation_consideration')      
+               
+        
+        
+    #If “No” to Q1, provide short answer stem question “Why not?”
+    def validate_why_partner_upsent_required(self):
+        required_fields = ['why_partner_upsent', 'father_child_contact']
+        for field in required_fields:
+            self.required_if(NO,
+                            field='partner_present',
+                            field_required=field)
+        
+    def validate_not_living_with_partner_required(self):
+        self.required_if(NO,
+                         field='living_with_partner',
+                         field_required='why_not_living_with_partner') 
+           
+    # If “Yes” on Q6 go to Q7  If “No” skip to Q8    
+    def validate_disclose_status_required(self):
+        self.required_if(NO,
+                         field='disclosure_to_partner',
+                         field_required='disclose_status')  
+        
+    # If “Yes” to Q10, continue to Q11. Otherwise skip to Q12 
+    
+    def validate_times_separated_required(self):
+        self.required_if(NO,
+                         field='ever_separated',
+                         field_required='times_separated')  
+        
+        
+    # validate on Q1 no
+    def validate_partner_upsent_fields_not_required(self):
+        not_required_fields = ['is_partner_the_father',
+                               'duration_with_partner_months',
+                               'duration_with_partner_years',
+                               'partner_age_in_years',
+                               'living_with_partner',
+                               'why_not_living_with_partner',
+                               'disclosure_to_partner',
+                               'discussion_with_partner',
+                               'disclose_status',
+                               'partners_support',
+                               'ever_separated',
+                               'times_separated',
+                               'separation_consideration',
+                               'after_fight',
+                               'relationship_progression',
+                               'confide_in_partner',
+                               'relationship_regret',
+                               'quarrel_frequency',
+                               'bothering_partner',
+                               'kissing_partner',
+                               'engage_in_interests',
+                               'happiness_in_relationship',
+                               'future_relationship',
+                               ]
+
+        for not_required in not_required_fields:
+            self.not_required_if(
+                NO,
+                field='partner_present',
+                field_required=not_required,
+                # inverse=False)
+        
+        
+        
+        
--- flourish_form_validations/tests/test_father_involvement_form.py
+++ flourish_form_validations/tests/test_father_involvement_form.py
@@ -32,7 +32,7 @@ class TestRelationshipFatherInvolment(TestModeMixin,TestCase):
         appointment = Appointment.objects.create(
             subject_identifier=self.subject_consent.subject_identifier,
             appt_datetime=get_utcnow(),
-            visit_code='2001M')
+            visit_code='1000')
 
         self.maternal_visit = MaternalVisit.objects.create(
             appointment=appointment,
@@ -41,23 +41,88 @@ class TestRelationshipFatherInvolment(TestModeMixin,TestCase):
 
         self.options = {
             'maternal_visit': self.maternal_visit,
+
         }
+        
+        # self.data = {
+            # 'maternal_visit': self.maternal_visit,
+            # 'partner_present':YES,
+            # 'why_partner_upsent':'test'
+            # 'is_partner_the_father':YES,
+            # 'duration_with_partner': '12',
+            # 'partner_age_in_years':'30',
+            # 'living_with_partner':YES,
+            # 'not_living_with_partner':,
+            # 'disclosure_to_partner':,
+            # 'discussion_with_partner':,
+            # 'disclose_status':,
+            # 'partners_support':,
+            # 'ever_separated':,
+            # 'times_separated':,
+            # 'separation_consideration':,
+            # 'after_fight':,
+            # 'relationship_progression':,
+            # 'confide_in_partner':,
+            # 'relationship_regret':,
+            # 'quarrel_frequency':,
+            # 'bothering_partner':,
+            # 'kissing_partner':,
+            # 'engage_in_interests':,
+            # 'happiness_in_relationship':,
+            # 'future_relationship':,
+            # 'father_child_contact':,
+            # 'fathers_financial_support':,
+            # 'child_left_alone':,
+            # 'read_books':,
+            # 'told_stories':,
+            # 'sang_songs':,
+            # 'took_child_outside':,
+            # 'played_with_child':,
+            # 'named_with_child':
+            # }
         
         """Test criteria's
         -form valid
-        -Q1
-        -Q5
-        -Q6
-        -Q10
-        -Q25
-        """
+                """
     def test_father_involvement_form_valid(self):
         
         cleaned_data = {
             'maternal_visit': self.maternal_visit,
-            'all_measurements': NO,
+            'partner_present':YES,
+            'is_partner_the_father':YES,
+            'duration_with_partner_months': '1',
+            'duration_with_partner_years': '1',
+            'partner_age_in_years':'30',
+            'living_with_partner':YES,
+            'disclosure_to_partner':YES,
+            'discussion_with_partner': 'easy',
+            'disclose_status':YES,
+            'partners_support':'supportive',
+            'ever_separated':YES,
+            'times_separated':'twice',
+            'separation_consideration':'occasionally',
             
-        }    
+            'after_fight':'occasionally',
+            'relationship_progression':'occasionally',
+            'confide_in_partner':'occasionally',
+            'relationship_regret':'occasionally',
+            'quarrel_frequency':'occasionally',
+            'bothering_partner':'occasionally',
+            'kissing_partner':'occasionally',
+            'engage_in_interests':'occasionally',
+            
+            'happiness_in_relationship':'happy',
+            'future_relationship':'happy',
+            'father_child_contact':'every_day',
+            'fathers_financial_support':'supportive',
+            'child_left_alone':3,
+            'read_books':'mother',
+            'told_stories':'mother',
+            'sang_songs':'mother',
+            'took_child_outside':'mother',
+            'played_with_child':'mother',
+            'named_with_child':'mother',
+        } 
             
         form_validator = RelationshipFatherInvolmentFormValidator(
             cleaned_data=cleaned_data)
@@ -65,7 +130,32 @@ class TestRelationshipFatherInvolment(TestModeMixin,TestCase):
             form_validator.validate()
         except ValidationError as e:
             self.fail(f'ValidationError unexpectedly raised. Got{e}') 
+        
+        
+        """ Validations
+        
+        -If ‘Yes’ to Q1, continue to Q2, 
+        -If “No” to Q1, provide short answer stem question “Why not?” (allow free text) otherwise skip to Q23
+        -If “No” to Q5, provide short answer stem question “Why not?” (allow free text) otherwise skip to Q6
+        -If “Yes” on Q6 go to Q7
+            If “No” skip to Q8
+        -If “Yes” to Q10, continue to Q11. Otherwise skip to Q12
+
+
+        -test validate_why_partner_upsent_required on YES and NO
+        -test validate_why_not_living_with_partner
+        -test validate_is_partner_the_father_required
+        -test validate_not_living_with_partner_required
+        -test validate_discussion_with_partner_required
+        -test validate_disclose_status_required
+        -test validate_times_separated_required
+        -test validate_separation_consideration_required
+
+
+        """
+
     
+
         
         
         
