7309ca2eb47e14c935fa67ebfed96455d5c402a8
salahdin <salahdinjemal@gmail.com>
2022-11-16 17:02:51 +0200

    add validator tb encounter screen version 2

---
+++ flourish_form_validations/form_validators/tb_routine_health_screen_version_2_form_validator.py
@@ -0,0 +1,23 @@
+from edc_constants.constants import YES
+from edc_form_validators import FormValidator
+from .crf_form_validator import FormValidatorMixin
+
+
+class TbRoutineHealthScreenVersionTwoFormValidator(FormValidatorMixin, FormValidator):
+
+    def clean(self):
+        super().clean()
+
+        fields_required = ['tb_screened', 'diagnostic_referral']
+
+        for field in fields_required:
+            self.required_if(
+                *['1', '2', '3', '4', '6'],
+                field='tb_health_visits',
+                field_required=field
+            )
+
+        self.validate_other_specify(
+            field='screen_location',
+            other_specify_field='screen_location'
+        )
