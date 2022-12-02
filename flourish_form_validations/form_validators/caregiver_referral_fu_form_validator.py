79ea386c9fb74a027201a7a558f91671a33c7078
Itumeleng Mosweu <imosweuk@gmail.com>
2022-12-02 15:37:53 +0200

    update referral follow up validations

--- flourish_form_validations/form_validators/caregiver_referral_fu_form_validator.py
+++ flourish_form_validations/form_validators/caregiver_referral_fu_form_validator.py
@@ -13,68 +13,97 @@ class CaregiverReferralFUFormValidator(FormValidatorMixin, FormValidator):
             'maternal_visit').subject_identifier
         super().clean()
 
-        pnta_fields = ['emo_support_type', 'emo_health_improved',
-                       'percieve_counselor', 'satisfied_counselor',
-                       'additional_counseling']
-
-        for p_field in pnta_fields:
-            self.not_required_if('PNTA',
-                                 field='emo_support_provider',
-                                 field_required=p_field,
-                                 inverse=False)
-
-        qs = self.cleaned_data.get('emo_health_improved')
-        if self.cleaned_data.get('emo_support_provider') == 'PNTA' and qs:
-            message = {
-                'emo_health_improved': 'This field is not required.'}
-            self._errors.update(message)
-            raise ValidationError(message)
-
-        other_fields = ['referred_to', 'support_ref_decline_reason',
-                        'no_support_reason', 'emo_support_type', 'percieve_counselor']
-
-        for other_field in other_fields:
-            self.validate_other_specify(field=other_field)
+        self.validate_other_specify(field='percieve_counselor')
 
         self.required_if(NO,
                          field='satisfied_counselor',
                          field_required='additional_counseling')
 
-        if (self.cleaned_data.get('emo_support_provider')
-                and self.cleaned_data.get('emo_support_provider') != 'PNTA'):
+        if self.cleaned_data.get('attended_referral'):
+            self.referral_specific_validations()
 
-            self.required_if(NO,
-                             field='attended_referral',
-                             field_required='support_ref_decline_reason')
+        if self.cleaned_data.get('emo_support_provider'):
+            self.referral_fu_specific_validations()
 
-            self.required_if(YES,
-                             field='attended_referral',
-                             field_required='emo_support')
+        self.m2m_other_specify(OTHER,
+                               m2m_field='emo_support_type',
+                               field_other='emo_support_type_other')
 
-            self.required_if(NO,
+        self.m2m_other_specify(OTHER,
+                               m2m_field='emo_health_improved',
+                               field_other='emo_health_improved_other')
+
+    def referral_specific_validations(self):
+
+        self.required_if(NO,
+                         field='attended_referral',
+                         field_required='support_ref_decline_reason')
+
+        self.required_if(YES,
+                         field='attended_referral',
+                         field_required='emo_support')
+
+        self.m2m_required_if(YES,
                              field='emo_support',
-                             field_required='no_support_reason')
+                             m2m_field='emo_support_type')
 
-            self.m2m_required_if(YES,
-                                 field='emo_support',
-                                 m2m_field='emo_support_type')
+        other_fields = ['referred_to', 'support_ref_decline_reason',
+                        'no_support_reason']
 
-            self.m2m_other_specify(OTHER,
-                                   m2m_field='emo_support_type',
-                                   field_other='emo_support_type_other')
+        for other_field in other_fields:
+            self.validate_other_specify(field=other_field)
 
-            self.m2m_other_specify(OTHER,
-                                   m2m_field='emo_health_improved',
-                                   field_other='emo_health_improved_other')
+        self.required_if(NO,
+                         field='emo_support',
+                         field_required='no_support_reason')
 
-            emo_fields = ['percieve_counselor',
-                          'satisfied_counselor']
+        emo_fields = ['percieve_counselor',
+                      'satisfied_counselor']
 
-            for emo in emo_fields:
-                self.required_if(YES,
-                                 field='emo_support',
-                                 field_required=emo)
+        for emo in emo_fields:
+            self.required_if(YES,
+                             field='emo_support',
+                             field_required=emo)
 
-            self.m2m_required_if(YES,
-                                 field='emo_support',
-                                 m2m_field='emo_health_improved')
+        self.m2m_required_if(YES,
+                             field='emo_support',
+                             m2m_field='emo_health_improved')
+
+    def referral_fu_specific_validations(self):
+
+        pnta_fields = ['emo_support_type', 'emo_health_improved',
+                       'percieve_counselor', 'satisfied_counselor']
+
+        for p_field in pnta_fields:
+            self.not_required_if('PNTA',
+                                 field='emo_support_provider',
+                                 field_required=p_field)
+
+        self.m2m_not_required_if('PNTA',
+                                 field='emo_support_provider',
+                                 m2m_field='emo_support_type',)
+
+        self.m2m_not_required_if('PNTA',
+                                 field='emo_support_provider',
+                                 m2m_field='emo_health_improved',)
+
+    def m2m_not_required_if(self, response=None, field=None, m2m_field=None):
+        """Raises an exception or returns False.
+
+        m2m_field is required if field  == response
+        """
+        message = None
+        if (self.cleaned_data.get(field) == response
+                and self.cleaned_data.get(m2m_field)):
+            message = {m2m_field: 'This field is not required'}
+        elif (self.cleaned_data.get(field) == response
+              and self.cleaned_data.get(m2m_field).count() != 0):
+            message = {m2m_field: 'This field is not required'}
+        elif (self.cleaned_data.get(field) != response
+              and self.cleaned_data.get(m2m_field)
+              and self.cleaned_data.get(m2m_field).count() == 0):
+            message = {m2m_field: 'This field is required'}
+        if message:
+            self._errors.update(message)
+            raise ValidationError(message)
+        return False
