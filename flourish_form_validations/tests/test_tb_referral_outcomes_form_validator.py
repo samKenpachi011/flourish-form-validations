dd0abcdb63dee7f9d2e2c03832b0f63f8e189995
salahdin <salahdinjemal@gmail.com>
2022-12-08 08:56:09 +0200

    add additional validations

--- flourish_form_validations/tests/test_tb_referral_outcomes_form_validator.py
+++ flourish_form_validations/tests/test_tb_referral_outcomes_form_validator.py
@@ -1,11 +1,9 @@
-from dateutil.relativedelta import relativedelta
 from django.core.exceptions import ValidationError
 from django.test import TestCase, tag
-from edc_base.utils import get_utcnow
 from edc_constants.constants import YES, NO, OTHER
 
 from ..form_validators import TbReferralOutcomesFormValidator
-from .models import FlourishConsentVersion, SubjectConsent, ListModel
+from .models import ListModel
 from .test_model_mixin import TestModeMixin
 
 
@@ -14,55 +12,15 @@ class TestTbReferralOutcomesFormValidator(TestModeMixin, TestCase):
     def __init__(self, *args, **kwargs):
         super().__init__(TbReferralOutcomesFormValidator, *args, **kwargs)
 
-    def setUp(self):
-        FlourishConsentVersion.objects.create(
-            screening_identifier='ABC12345')
-
-        self.subject_identifier = '11111111'
-
-        self.subject_consent = SubjectConsent.objects.create(
-            subject_identifier='11111111', screening_identifier='ABC12345',
-            gender='M', dob=(get_utcnow() - relativedelta(years=25)).date(),
-            consent_datetime=get_utcnow(), version='1')
-
-    def test_referral_clinic_appt_required(self):
+    def test_referral_clinic_appt_required_valid(self):
         """
-        Raise error if referral_clinic_appt is YES and fields 'tb_diagnostic_perf',
+        Raise error if referral_clinic_appt is Yes and fields further_tb_eval , tb_diagnostic_perf,
          'tb_treat_start', 'tb_prev_therapy_start' are null
         """
-
+        ListModel.objects.create(short_name="sputum")
         cleaned_data = {
             'referral_clinic_appt': YES,
-        }
-
-        form_validator = TbReferralOutcomesFormValidator(
-            cleaned_data=cleaned_data)
-
-        try:
-            form_validator.validate()
-        except ValidationError as e:
-            self.assertRaises(ValidationError)
-
-    def test_referral_clinic_appt_not_required(self):
-        """
-        Raise error if referral_clinic_appt is NO and other fields are provided
-        """
-
-        cleaned_data = {
-            'referral_clinic_appt': NO,
-        }
-
-        form_validator = TbReferralOutcomesFormValidator(
-            cleaned_data=cleaned_data)
-        try:
-            form_validator.validate()
-        except ValidationError as e:
-            self.fail(f'ValidationError unexpectedly raised. Got{e}')
-
-    def test_tb_diagnostics_valid(self):
-        ListModel.objects.create(short_name="septum")
-        cleaned_data = {
-            'referral_clinic_appt': YES,
+            'further_tb_eval': YES,
             'tb_diagnostic_perf': YES,
             'tb_treat_start': NO,
             'tb_prev_therapy_start': YES,
@@ -78,11 +36,37 @@ class TestTbReferralOutcomesFormValidator(TestModeMixin, TestCase):
         except ValidationError as e:
             self.fail(f'ValidationError unexpectedly raised. Got{e}')
 
-    def test_tb_diagnostics_other_specify_valid(self):
-        ListModel.objects.create(short_name=OTHER)
+    def test_referral_clinic_appt_required_invalid(self):
+        """
+        Raise error if referral_clinic_appt is No and fields further_tb_eval , tb_diagnostic_perf,
+         'tb_treat_start', 'tb_prev_therapy_start' are not null
+        """
+        ListModel.objects.create(short_name="sputum")
+        cleaned_data = {
+            'referral_clinic_appt': NO,
+            'further_tb_eval': None,
+            'tb_diagnostic_perf': YES,
+            'tb_treat_start': NO,
+            'tb_prev_therapy_start': YES,
+            'tb_diagnose_pos': YES,
+            'tb_test_results': "thiiis ",
+            'tb_diagnostics': ListModel.objects.all()
+        }
 
+        form_validator = TbReferralOutcomesFormValidator(
+            cleaned_data=cleaned_data)
+
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('tb_diagnostic_perf', form_validator._errors)
+
+    def test_tb_diagnostics_other_specify(self):
+        """
+         Raise error if tb_diagnostics is Other and tb_diagnostics_other is null
+         """
+        ListModel.objects.create(short_name=OTHER)
         cleaned_data = {
             'referral_clinic_appt': YES,
+            'further_tb_eval': YES,
             'tb_diagnostic_perf': YES,
             'tb_treat_start': NO,
             'tb_prev_therapy_start': YES,
@@ -93,27 +77,73 @@ class TestTbReferralOutcomesFormValidator(TestModeMixin, TestCase):
         }
         form_validator = TbReferralOutcomesFormValidator(
             cleaned_data=cleaned_data)
-        try:
-            form_validator.validate()
-        except ValidationError as e:
-            self.fail(f'ValidationError unexpectedly raised. Got{e}')
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('tb_diagnostics_other', form_validator._errors)
 
-    def test_tb_diagnostics_other_specify_invalid(self):
-        ListModel.objects.create(short_name=OTHER)
+    def test_referral_tb_diagnostic_perf_not_required(self):
+        """
+        Raise error if tb_diagnostic_perf is NO and tb_diagnose_pos is provided
+        """
 
         cleaned_data = {
             'referral_clinic_appt': YES,
+            'further_tb_eval': YES,
+            'tb_diagnostic_perf': NO,
+            'tb_treat_start': NO,
+            'tb_prev_therapy_start': YES,
+            'tb_diagnose_pos': YES,
+            'tb_test_results': "thiiis ",
+            'tb_diagnostics': ListModel.objects.all(),
+            'tb_diagnostics_other': None,
+        }
+
+        form_validator = TbReferralOutcomesFormValidator(
+            cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('tb_diagnose_pos', form_validator._errors)
+
+    def test_referral_tb_diagnostic_perf_required(self):
+        """
+        Raise error if tb_diagnostic_perf is Yes and tb_diagnose_pos is provided
+        """
+        ListModel.objects.create(short_name="sputum")
+        cleaned_data = {
+            'referral_clinic_appt': YES,
+            'further_tb_eval': YES,
             'tb_diagnostic_perf': YES,
             'tb_treat_start': NO,
             'tb_prev_therapy_start': YES,
             'tb_diagnose_pos': YES,
             'tb_test_results': "thiiis ",
             'tb_diagnostics': ListModel.objects.all(),
-            'tb_diagnostics_other': "thiiis",
+            'tb_diagnostics_other': None,
         }
+
         form_validator = TbReferralOutcomesFormValidator(
             cleaned_data=cleaned_data)
         try:
             form_validator.validate()
         except ValidationError as e:
             self.fail(f'ValidationError unexpectedly raised. Got{e}')
+
+    def test_referral_tb_diagnose_pos_required(self):
+        """
+        Raise error if tb_diagnose_pos is No and tb_test_results is provided
+        """
+        ListModel.objects.create(short_name="sputum")
+        cleaned_data = {
+            'referral_clinic_appt': YES,
+            'further_tb_eval': YES,
+            'tb_diagnostic_perf': YES,
+            'tb_treat_start': NO,
+            'tb_prev_therapy_start': YES,
+            'tb_diagnose_pos': NO,
+            'tb_test_results': "thiiis ",
+            'tb_diagnostics': ListModel.objects.all(),
+            'tb_diagnostics_other': None,
+        }
+
+        form_validator = TbReferralOutcomesFormValidator(
+            cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('tb_test_results', form_validator._errors)
