721e2c556cdd7009b921a20a75104299a85ca091
salahdin <salahdinjemal@gmail.com>
2022-12-12 15:46:22 +0200

    refactor materanl arv post adherence validator

--- flourish_form_validations/form_validators/maternal_arv_post_adherence_form_validator.py
+++ flourish_form_validations/form_validators/maternal_arv_post_adherence_form_validator.py
@@ -9,30 +9,18 @@ class MaternalArvPostAdherenceFormValidator(FormValidatorMixin, FormValidator):
     def clean(self):
         super().clean()
 
-        self.validate_interruption_reason_against_missed_arv()
+        self.validate_interruption_reason_against_missed_arv(self.cleaned_data)
 
         self.validate_other_specify(
             field='interruption_reason',
             other_specify_field='interruption_reason_other'
         )
 
-    def validate_interruption_reason_against_missed_arv(self):
+    def validate_interruption_reason_against_missed_arv(self, cleaned_data):
 
-        reason = ['TOXICITY_SELF', 'TOXICITY_HEALTHCARE_PROVIDER', 'NO_DRUGS',
-                  'NO_REFILL', 'FORGOT', 'TRAVELING', 'DEFAULT', OTHER]
-
-        if (self.cleaned_data.get('missed_arv') == 0 and
-                self.cleaned_data.get('interruption_reason') in reason):
-
-            message = {'interruption_reason': 'can\'t choose this option'
-                                              ' when participant has no missed arvs'}
-            self._errors.update(message)
-            raise ValidationError(message)
-
-        elif (self.cleaned_data.get('missed_arv') >= 1 and
-              self.cleaned_data.get('interruption_reason') == NOT_APPLICABLE):
-
-            message = {'interruption_reason': 'can\'t choose this option'
-                                              ' when participant has more than one misses'}
-            self._errors.update(message)
-            raise ValidationError(message)
+        self.applicable_if_true(
+            cleaned_data.get('missed_arv') >= 1,
+            field_applicable='interruption_reason',
+            applicable_msg='Can\'t choose this option when participant has more than one misses',
+            not_applicable_msg='Can\'t choose this option when participant has no missed arvs'
+        )
