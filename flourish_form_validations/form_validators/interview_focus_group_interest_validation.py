9ae92672fe8040cd1db0ad67612f5be5ba24e46d
salahdin <salahdinjemal@gmail.com>
2023-04-27 11:50:53 +0200

    add validation for within first year postpartum

--- flourish_form_validations/form_validators/interview_focus_group_interest_validation.py
+++ flourish_form_validations/form_validators/interview_focus_group_interest_validation.py
@@ -1,3 +1,6 @@
+from datetime import timedelta
+from edc_base.utils import get_utcnow
+
 from django.core.exceptions import ValidationError
 from edc_form_validators import FormValidator
 from .crf_form_validator import FormValidatorMixin
@@ -26,20 +29,23 @@ class InterviewFocusGroupInterestFormValidator(FormValidatorMixin, FormValidator
             field_required='hiv_group_pref',
         )
 
-        condition = self.is_preg_enroll()
-
         self.required_if_true(
-            condition,
+            self.is_preg_enroll(),
             field_required='infant_feeding_group_interest',
             required_msg='Infant feeding group interest is required if enrolled'
                          ' pregnant(pregnant or postpartum).',
         )
 
         fields = ['same_status_comfort', 'diff_status_comfort']
+        condition = (
+                self.is_preg_enroll
+                and self.cleaned_data['discussion_pref'] in ['group', 'either']
+                and self.is_within_first_year_postpartum()
+        )
 
         for field in fields:
             self.required_if_true(
-                self.is_preg_enroll and self.cleaned_data['discussion_pref'] in ['group', 'either'],
+                condition,
                 field_required=field,
             )
 
@@ -53,3 +59,20 @@ class InterviewFocusGroupInterestFormValidator(FormValidatorMixin, FormValidator
             return consent.preg_enroll
         else:
             raise ValidationError('Caregiver consent on behalf of child does not exist.')
+
+    def is_within_first_year_postpartum(self):
+        """Returns True if subject is currently in first year postpartum."""
+
+        maternal_visit = self.cleaned_data.get('maternal_visit')
+        subject_identifier = maternal_visit.subject_identifier
+        maternal_delivery_objs = self.maternal_delivery_model_cls.objects.filter(
+            subject_identifier=subject_identifier
+        )
+        if maternal_delivery_objs.exists():
+            maternal_delivery_obj = maternal_delivery_objs.first()
+            if maternal_delivery_obj.delivery_datetime:
+                today = get_utcnow().date()
+                delivery_date = maternal_delivery_obj.delivery_datetime.date()
+                return (today - delivery_date) < timedelta(days=365)
+        else:
+            return False
