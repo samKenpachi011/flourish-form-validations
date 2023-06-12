37c6899684fbab28f662cbf27abc19738251c36b
Moses Chawawa <moseschawawa@gmail.com>
2023-06-12 11:49:12 +0200

    Post hiv validations

---
+++ flourish_form_validations/form_validators/post_hiv_rapid_testing_and_conseling_form_validator.py
@@ -0,0 +1,41 @@
+from dateutil.relativedelta import relativedelta
+from django.core.exceptions import ValidationError
+from edc_constants.constants import YES, NO
+from edc_form_validators import FormValidator
+
+from .crf_form_validator import FormValidatorMixin
+
+
+class PostHIVRapidTestCounselingFormValidator(FormValidatorMixin, FormValidator):
+
+    def clean(self):
+        self.subject_identifier = self.cleaned_data.get(
+            'maternal_visit').subject_identifier
+        super().clean()
+
+        for field in ['result_date', 'result']:
+
+            self.required_if(
+                YES,
+                field='rapid_test_done',
+                field_required=field,)
+
+        self.validate_test_date(self.cleaned_data.get('result_date'),)
+
+        self.required_if(
+            NO,
+            field='rapid_test_done',
+            field_required='reason_not_tested',)
+
+        self.validate_other_specify(field='reason_not_tested')
+
+    def validate_test_date(self, test_date=None):
+
+        maternal_visit = self.cleaned_data.get('maternal_visit')
+
+        if test_date and relativedelta(
+                maternal_visit.report_datetime.date(), test_date).months >= 3:
+            message = {
+                'result_date': 'The date provided is more than 3 months old.'}
+            self._errors.update(message)
+            raise ValidationError(message)
---
+++ flourish_form_validations/tests/test_post_hiv_rapid_testing_and_conseling_from_validator.py
@@ -0,0 +1,59 @@
+
+from datetime import timedelta
+from django.test import TestCase, tag
+from django.core.exceptions import ValidationError
+from edc_base.utils import get_utcnow
+
+from .test_model_mixin import TestModeMixin
+from ..form_validators import PostHIVRapidTestCounselingFormValidator
+from .models import FlourishConsentVersion, SubjectConsent, Appointment, MaternalVisit
+from dateutil.relativedelta import relativedelta
+
+
+class TestPostHIVRapidTestCounselingFormValidator(TestModeMixin, TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(PostHIVRapidTestCounselingFormValidator, *args, **kwargs)
+
+    def setUp(self):
+
+        FlourishConsentVersion.objects.create(
+            screening_identifier='ABC12345')
+
+        subject_consent = SubjectConsent.objects.create(
+            subject_identifier='11111111', screening_identifier='ABC12345',
+            gender='M', dob=(get_utcnow() - relativedelta(years=25)).date(),
+            consent_datetime=get_utcnow())
+
+        appointment = Appointment.objects.create(
+            subject_identifier=subject_consent.subject_identifier,
+            appt_datetime=get_utcnow(),
+            visit_code='3000')
+
+        self.maternal_visit = MaternalVisit.objects.create(
+            appointment=appointment,
+            subject_identifier=appointment.subject_identifier,
+            report_datetime=get_utcnow())
+
+        self.cleaned_data = {
+            'report_datetime': get_utcnow(),
+            'subject_identifier': appointment.subject_identifier,
+            'maternal_visit': self.maternal_visit,
+            'rapid_test_done': 'Yes',
+            'result_date': get_utcnow().date(),
+            'result': 'POS',
+            'reason_not_tested': None,
+            'reason_not_tested_other': None,
+            'comment': ''
+        }
+
+    def test_result_date_within_3months(self):
+
+        form_validator = PostHIVRapidTestCounselingFormValidator(
+            cleaned_data=self.cleaned_data)
+
+        result_date = get_utcnow().date() - relativedelta(months=4)
+
+        self.assertRaises(
+            ValidationError, form_validator.validate_test_date, result_date)
+        self.assertIn('result_date', form_validator._errors)
