97ad65b2656ea6dcf96820e0eb548dc349f96633
Ame Diphoko <diphokoame@gmail.com>
2023-06-30 06:14:14 +0200

    fix: ultrasound gestations validations for int/decimal fields

--- flourish_form_validations/tests/test_ultrasound_form.py
+++ flourish_form_validations/tests/test_ultrasound_form.py
@@ -36,91 +36,103 @@ class TestUltrasoundForm(TestModeMixin, TestCase):
             subject_identifier=self.subject_consent.subject_identifier,
             report_datetime=get_utcnow())
 
+        self.cleaned_data = {'report_datetime': get_utcnow(),
+                             'bpd': 4.2,
+                             'hc': 3.1,
+                             'ac': 2.1,
+                             'fl': 3.2,
+                             'amniotic_fluid_volume': '0',
+                             'ga_by_lmp': 0,
+                             'ga_by_ultrasound_wks': 0,
+                             'ga_by_ultrasound_days': 0,
+                             'est_fetal_weight': 0,
+                             'ga_confirmed': 0,
+                             'est_edd_ultrasound': get_utcnow().date(), }
+
     def test_est_ultrasound_less_than(self):
-        cleaned_data = {
+        self.cleaned_data.update({
             'maternal_visit': self.maternal_visit,
             'est_edd_ultrasound': get_utcnow().date() + relativedelta(days=50),
             'report_datetime': get_utcnow()
-        }
+        })
         form_validator = UltrasoundFormValidator(
-            cleaned_data=cleaned_data)
+            cleaned_data=self.cleaned_data)
         try:
             form_validator.validate()
         except ValidationError as e:
             self.fail(f'ValidationError unexpectedly raised. Got{e}')
 
     def test_est_ultrasound_greater_than(self):
-        cleaned_data = {
+        self.cleaned_data.update({
             'maternal_visit': self.maternal_visit,
             'est_edd_ultrasound': get_utcnow().date() + relativedelta(weeks=60),
             'report_datetime': get_utcnow(),
-        }
+        })
         form_validator = UltrasoundFormValidator(
-            cleaned_data=cleaned_data)
+            cleaned_data=self.cleaned_data)
         self.assertRaises(ValidationError, form_validator.validate)
         self.assertIn('est_edd_ultrasound', form_validator._errors)
 
     def test_est_ultrasound_less_than_invalid(self):
-        cleaned_data = {
+        self.cleaned_data.update({
             'maternal_visit': self.maternal_visit,
             'est_edd_ultrasound': get_utcnow().date(),
             'report_datetime': get_utcnow()
-        }
+        })
         form_validator = UltrasoundFormValidator(
-            cleaned_data=cleaned_data)
+            cleaned_data=self.cleaned_data)
         try:
             form_validator.validate()
         except ValidationError as e:
             self.fail(f'ValidationError unexpectedly raised. Got{e}')
 
     def test_ga_ultrasound_wks_valid(self):
-        cleaned_data = {
+        self.cleaned_data.update({
             'maternal_visit': self.maternal_visit,
             'ga_by_ultrasound_wks': 35,
             'report_datetime': get_utcnow(),
             'est_edd_ultrasound': get_utcnow().date() + relativedelta(weeks=5),
-
-        }
+        })
         form_validator = UltrasoundFormValidator(
-            cleaned_data=cleaned_data)
+            cleaned_data=self.cleaned_data)
         try:
             form_validator.validate()
         except ValidationError as e:
             self.fail(f'ValidationError unexpectedly raised. Got{e}')
 
     def test_ga_ultrasound_wks_invalid(self):
-        cleaned_data = {
+        self.cleaned_data.update({
             'maternal_visit': self.maternal_visit,
             'ga_by_ultrasound_wks': 50,
             'report_datetime': get_utcnow(),
-        }
+        })
         form_validator = UltrasoundFormValidator(
-            cleaned_data=cleaned_data)
+            cleaned_data=self.cleaned_data)
         self.assertRaises(ValidationError, form_validator.validate)
         self.assertIn('ga_by_ultrasound_wks', form_validator._errors)
 
     def test_ga_by_ultrasound_days_valid(self):
-        cleaned_data = {
+        self.cleaned_data.update({
             'maternal_visit': self.maternal_visit,
             'ga_by_ultrasound_days': 6,
             'report_datetime': get_utcnow(),
-        }
+        })
         form_validator = UltrasoundFormValidator(
-            cleaned_data=cleaned_data)
+            cleaned_data=self.cleaned_data)
         try:
             form_validator.validate()
         except ValidationError as e:
             self.fail(f'ValidationError unexpectedly raised. Got{e}')
 
     def test_ga_by_ultrasound_against_est_edd_ultrasound_valid(self):
-        cleaned_data = {
+        self.cleaned_data.update({
             'maternal_visit': self.maternal_visit,
             'est_edd_ultrasound': get_utcnow().date() + relativedelta(weeks=5),
             'ga_by_ultrasound_wks': 35,
             'report_datetime': get_utcnow()
-        }
+        })
         form_validator = UltrasoundFormValidator(
-            cleaned_data=cleaned_data)
+            cleaned_data=self.cleaned_data)
         try:
             form_validator.validate()
         except ValidationError as e:
@@ -130,13 +142,13 @@ class TestUltrasoundForm(TestModeMixin, TestCase):
         """
         check if bpd value allows for values below 5
         """
-        cleaned_data = {
+        self.cleaned_data.update({
             'maternal_visit': self.maternal_visit,
             'bpd': 2,
-        }
+        })
 
         form_validator = UltrasoundFormValidator(
-            cleaned_data=cleaned_data)
+            cleaned_data=self.cleaned_data)
         try:
             form_validator.validate()
         except ValidationError as e:
