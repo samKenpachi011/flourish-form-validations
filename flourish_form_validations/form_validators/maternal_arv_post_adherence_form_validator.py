d0519a58ffa6cb6f5fcc4fc792493ec8d6301be1
Ame Diphoko <diphokoame@gmail.com>
2023-10-06 15:34:34 +0200

    update: validations for v2 of post adherence crf

--- flourish_form_validations/form_validators/maternal_arv_post_adherence_form_validator.py
+++ flourish_form_validations/form_validators/maternal_arv_post_adherence_form_validator.py
@@ -1,4 +1,6 @@
+from edc_constants.constants import NOT_APPLICABLE, YES, OTHER
 from edc_form_validators import FormValidator
+
 from .crf_form_validator import FormValidatorMixin
 
 
@@ -8,16 +10,40 @@ class MaternalArvPostAdherenceFormValidator(FormValidatorMixin, FormValidator):
         super().clean()
 
         self.validate_interruption_reason_against_missed_arv(self.cleaned_data)
-        self.validate_other_specify(
-            field='interruption_reason',
-            other_specify_field='interruption_reason_other'
-        )
+
+        self.m2m_other_specify(
+            OTHER,
+            m2m_field='interruption_reason',
+            field_other='interruption_reason_other')
+
+        self.required_if(
+            YES,
+            field='stopped_art_past_yr',
+            field_required='stopped_art_freq')
+
+        stopped_art = self.cleaned_data.get('stopped_art_past_yr', None)
+        self.m2m_applicable_if_true(
+            stopped_art == YES,
+            m2m_field='stopped_art_reasons')
+
+        self.m2m_other_specify(
+            OTHER,
+            m2m_field='stopped_art_reasons',
+            field_other='stopped_reasons_other')
+
+        selections = [NOT_APPLICABLE]
+        self.m2m_single_selection_if(
+            *selections,
+            m2m_field='stopped_art_reasons')
+
 
     def validate_interruption_reason_against_missed_arv(self, cleaned_data):
-        if cleaned_data.get('missed_arv') is not None:
-            self.applicable_if_true(
+        if cleaned_data.get('missed_arv', None) is not None:
+            self.m2m_applicable_if_true(
                 cleaned_data.get('missed_arv') >= 1,
-                field_applicable='interruption_reason',
-                applicable_msg='Can\'t choose this option when participant has more than one misses',
-                not_applicable_msg='Can\'t choose this option when participant has no missed arvs'
-            )
+                m2m_field='interruption_reason')
+
+        selections = [NOT_APPLICABLE]
+        self.m2m_single_selection_if(
+            *selections,
+            m2m_field='interruption_reason')
