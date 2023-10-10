2c17cf81cf0556f7ed28b2da003c191df0568013
Ame Diphoko <diphokoame@gmail.com>
2023-10-10 13:45:23 +0200

    add: extra validations for v2 questions post adherence

--- flourish_form_validations/form_validators/maternal_arv_post_adherence_form_validator.py
+++ flourish_form_validations/form_validators/maternal_arv_post_adherence_form_validator.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from edc_constants.constants import NOT_APPLICABLE, YES, OTHER
 from edc_form_validators import FormValidator
 
@@ -16,12 +17,20 @@ class MaternalArvPostAdherenceFormValidator(FormValidatorMixin, FormValidator):
             m2m_field='interruption_reason',
             field_other='interruption_reason_other')
 
+        stopped_art = self.cleaned_data.get('stopped_art_past_yr', None)
+        missed_arv = self.cleaned_data.get('missed_arv', None)
+
+        if missed_arv == 7 and stopped_art != YES:
+            message = {'stopped_art_past_yr':
+                       'Response should always be a "Yes" if Q3. is 7'}
+            self._errors.update(message)
+            raise ValidationError(message)
+
         self.required_if(
             YES,
             field='stopped_art_past_yr',
             field_required='stopped_art_freq')
 
-        stopped_art = self.cleaned_data.get('stopped_art_past_yr', None)
         self.m2m_applicable_if_true(
             stopped_art == YES,
             m2m_field='stopped_art_reasons')
